412702a5eab152be381b3a612901d840d8078a86
Josee <info@joseewouters.nl>
2021-01-10 14:46:10 +0100

    Correct grammar for dice

--- index.js
+++ index.js
@@ -3,20 +3,22 @@ toggleTheme = () => {
 	root.hasAttribute("data-theme") ? root.removeAttribute("data-theme") : root.setAttribute("data-theme", "dark");
 }
 
-rollDices = () => {
-	const dices = document.querySelectorAll(".dice");
+rollDice = () => {
+	const dice = document.querySelectorAll(".dice");
 	const diceSides = document.querySelectorAll(".dice__side");
 	diceSides.forEach(diceSide => {
 		diceSide.style.display = "none";
 	});
-	dices.forEach(dice => {
+	dice.forEach(die => {
 		const randomNumber = Math.floor(Math.random() * 6) + 1;
 		const randomSide = "dice__side--" + randomNumber;
-		const diceSide = dice.querySelector("." + randomSide);
+		const diceSide = die.querySelector("." + randomSide);
 		diceSide.style.display = "block";
 	});
 }
 
+
+
 document.addEventListener("DOMContentLoaded", function() {
-	rollDices();
+	rollDice();
 });
